15ef93d441a32f82fe6ecf4dc9703b1098a781d7
Matias Cabezas <bmatiasx@gmail.com>
2022-12-02 00:48:49 -0300

    Fixing log format

--- src/main/java/com/eurekalabs/stockmarketapi/controller/StockMarketController.java
+++ src/main/java/com/eurekalabs/stockmarketapi/controller/StockMarketController.java
@@ -29,22 +29,19 @@ public class StockMarketController {
 
     @GetMapping("/welcome")
     public ResponseEntity<?> welcome() {
-        time = LocalDateTime.now();
-        log.info(String.format("%s - welcome endpoint was called", time));
+        log.info("welcome endpoint was called");
         return new ResponseEntity<>("Welcome to Stock Market API", HttpStatus.OK);
     }
 
     @PostMapping(path = "/api-key")
     @ResponseBody
     public ResponseEntity<Object> getApiKey(@RequestBody User user) {
-        time = LocalDateTime.now();
-
         Map<String, String> apiKey;
-        log.info(String.format("%s - api-key endpoint was called. Request payload: %s", time, user.toString()));
+        log.info(String.format("api-key endpoint was called. Request payload: %s",  user.toString()));
         try {
             apiKey = service.getApiKey(user);
         } catch (UserValidationException e) {
-            log.error(String.format("%s - validation error: %s", time, e.getMessage()));
+            log.error(String.format("validation error: %s",  e.getMessage()));
             return new ResponseEntity<>(e.getMessage(), null, HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(apiKey, HttpStatus.OK);
@@ -53,17 +50,16 @@ public class StockMarketController {
     @GetMapping("/price-intraday")
     public ResponseEntity<?> getStockPrice(@RequestParam("key") String apiKey, @RequestParam("function") String function,
                                            @RequestParam("symbol") String symbol, @RequestParam("interval") String interval) {
-        time = LocalDateTime.now();
-        log.info(String.format("%s - getStockPrice endpoint was called. Request params: key=%s, function=%s, symbol=%s, interval=%s", time, apiKey, function, symbol, interval));
+        log.info(String.format("getStockPrice endpoint was called. Request params: key=%s, function=%s, symbol=%s, interval=%s",  apiKey, function, symbol, interval));
 
         Price price;
         try {
             price = service.getPriceIntraday(apiKey, function, symbol, interval);
         } catch (UserValidationException e) {
-            log.error(String.format("%s - validation error: %s", time, e.getMessage()));
+            log.error(String.format("validation error: %s", e.getMessage()));
             return new ResponseEntity<>(e.getMessage(), null, HttpStatus.UNAUTHORIZED);
         } catch (Exception e) {
-            log.error(String.format("%s - internal server error: %s", time, e.getMessage()));
+            log.error(String.format("internal server error: %s", e.getMessage()));
             return new ResponseEntity<>(e.getMessage(), null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
